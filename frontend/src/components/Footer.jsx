90854e5fc4c8b62dc7a194fa0fda65078e81ab27
ClementPremartin <clementpremartin@gmail.com>
2022-04-15 09:28:09 +0200

    adding logo facebook in the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Footer.css";
-import "@assets/facebook.svg"
+import "@assets/facebook.svg";
 
 function Footer() {
   return (
     <div className="footer">
       <div>
-        <img src="./assets/facebook.svg" alt="logo facebook"></img>
+        <img src="./assets/facebook.svg" alt="logo facebook" />
       </div>
       <h1>test from footer</h1>
     </div>
