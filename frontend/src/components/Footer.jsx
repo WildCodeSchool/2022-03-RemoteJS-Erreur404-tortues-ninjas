7b9374fb79edc6a17f69631de9e0e981549aa981
ClementPremartin <clementpremartin@gmail.com>
2022-04-15 10:14:44 +0200

    adding style on the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import "./Footer.css";
-import "@assets/facebook.svg";
+import logoFacebook from "@assets/facebook.svg";
+import logoInsta from "@assets/instagram.svg";
+import logoTwitter from "@assets/twitter.svg";
 
 function Footer() {
   return (
-    <div className="footer">
-      <div>
-        <img src="./assets/facebook.svg" alt="logo facebook" />
+    <div>
+      <div className="footer">
+        <div className="images">
+          <div className="img">
+            <img src={logoFacebook} alt="logo facebook" />
+          </div>
+          <div className="img">
+            <img src={logoInsta} alt="logo insta" />
+          </div>
+          <div className="img">
+            <img src={logoTwitter} alt="logo twitter" />
+          </div>
+        </div>
+        <div className="global-links">
+          <div className="links">
+            <p>Mentions légales</p>
+          </div>
+          <div className="links">
+            <p>Politique de confidentialité</p>
+          </div>
+        </div>
       </div>
-      <h1>test from footer</h1>
     </div>
   );
 }
