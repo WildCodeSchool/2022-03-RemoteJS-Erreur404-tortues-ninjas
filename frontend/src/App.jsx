6b7b7258b106280621be0e943cb8d44037469285
WENDYBAQUE <BAQUE.WENDY@gmail.com>
2022-04-22 11:28:34 +0200

    Try to commit my changes to texte-presentation component and Tailwind styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,27 @@
 import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./pages/Header";
-// import Home from "./pages/Home";
-// import RechercheFilm from "./pages/RechercheFilm";
-// import RecherchePizza from "./pages/RecherchePizza";
-// import ResultatFilm from "./pages/ResultatFilm";
-// import ResultatPizza from "./pages/ResultatPizza";
-// import Formulaire from "./pages/Formulaire";
-// import Navbar from "./pages/Navbar";
-// import PopupSuggestion from "./pages/PopupSuggestion";
-// import PopupRetourAccueil from "./pages/PopupRetourAccueil";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Header from "./pages/Header";
+import Home from "./pages/Home";
+import RechercheFilm from "./pages/RechercheFilm";
+import RecherchePizza from "./pages/RecherchePizza";
+import ResultatFilm from "./pages/ResultatFilm";
+import ResultatPizza from "./pages/ResultatPizza";
+import Formulaire from "./pages/Formulaire";
+import Navbar from "./pages/Navbar";
+import PopupSuggestion from "./pages/PopupSuggestion";
+import PopupRetourAccueil from "./pages/PopupRetourAccueil";
+import TextePresentation from "./components/TextePresentation";
+import HomeButton from "./components/HomeButton";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      {/* <Router>
+      {/* <Header /> */}
+      <Router>
         <Navbar />
+        <TextePresentation />
+        <HomeButton />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search/film" element={<RechercheFilm />} />
@@ -28,7 +33,7 @@ function App() {
           <Route path="/popup/home" element={<PopupRetourAccueil />} />
           <Route path="/return" element={<PopupRetourAccueil />} />
         </Routes>
-      </Router> */}
+      </Router>
     </div>
   );
 }
