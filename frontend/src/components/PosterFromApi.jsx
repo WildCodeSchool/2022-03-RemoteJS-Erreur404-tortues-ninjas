abfdc5b9732198365be550ce239a7134fdb9337c
ClementPremartin <clementpremartin@gmail.com>
2022-04-25 18:24:27 +0200

    map poster array in PosterFromApi component

--- frontend/src/components/PosterFromApi.jsx
+++ frontend/src/components/PosterFromApi.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import "./PosterFromApi.css";
 
 function ApiRequests({ posterArray }) {
-  // console.warn(posterArray);
   const imageUrl = "https://image.tmdb.org/t/p/w500";
   return (
     <div className="global">
       <div className="movie-card">
         <img
           className="poster"
-          src={imageUrl + posterArray[1]}
+          src={imageUrl + posterArray.map((poster) => poster)}
           alt="movie poster"
         />
       </div>
