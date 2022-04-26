8379b7d54f5ca0ecb1b2d2c1609c4a0b89e7f6d9
WENDYBAQUE <BAQUE.WENDY@gmail.com>
2022-04-26 08:35:42 +0200

    Add personnalised fontFamily and colors to Tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   content: ["./src/**/*.jsx"],
   theme: {
+    colors: {
+      white: "#f0edee",
+      black: "#141414",
+      red: "b52030",
+      green: "90f0a9",
+    },
+    fontFamily: {
+      sans: ["Hind", "sans-serif"],
+      serif: ["Expletus Sans", "serif"],
+    },
     extend: {},
   },
   plugins: [],
