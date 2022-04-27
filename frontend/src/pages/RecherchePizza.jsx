a03fe62e4defc5681c0229e43b8767e64e0ec887
WENDYBAQUE <BAQUE.WENDY@gmail.com>
2022-04-27 14:50:24 +0200

    commit before checkout

--- frontend/src/pages/RecherchePizza.jsx
+++ frontend/src/pages/RecherchePizza.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import MoodLogo from "../assets/MoodLogo.png";
 import TitleBRPizza from "../components/TitleBRPizza";
 
 function RecherchePizza() {
   return (
     <div>
+      <div className="MoodLogo">
+        <img src={MoodLogo} alt="MoodLogo" />
+      </div>
       <TitleBRPizza />
     </div>
   );
