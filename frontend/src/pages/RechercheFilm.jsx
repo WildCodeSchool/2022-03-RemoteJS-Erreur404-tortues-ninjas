6f3964a6cdfd5b3dd8f77f9bfe8377a7dcb87f19
WENDYBAQUE <BAQUE.WENDY@gmail.com>
2022-04-27 15:29:14 +0200

    Add logo in the page recherche film

--- frontend/src/pages/RechercheFilm.jsx
+++ frontend/src/pages/RechercheFilm.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import MoodLogo from "../assets/MoodLogo.png";
 import TitleBRFilm from "../components/TitleBRFilm";
 
 function RechercheFilm() {
   return (
     <div>
+      <div className="MoodLogo">
+        <img src={MoodLogo} alt="MoodLogo" />
+      </div>
       <TitleBRFilm />
     </div>
   );
