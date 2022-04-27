d8386ebf2f54abb9901f03b22ae7800a43a3729e
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-04-27 15:54:23 +0200

    Delete CSS page resultat film

--- frontend/src/pages/ResultatFilm.jsx
+++ frontend/src/pages/ResultatFilm.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import "./ResultatFilm.css";
 import ResultatFilmComponent from "../components/ResultatFilmComponent";
 
 function ResultatFilm() {
   return (
     <div>
-      <h1>Résultat films</h1>
       <ResultatFilmComponent />
     </div>
   );
