8c4c784654a2923123db36ce91877553a801565a
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-04-27 16:06:26 +0200

    Delete classname <parent>

--- frontend/src/components/ResultatFilmComponent.jsx
+++ frontend/src/components/ResultatFilmComponent.jsx
@@ -4,16 +4,14 @@ import "./ResultatFilmComponent.css";
 
 function ResultatFilmComponent() {
   return (
-    <div className="parent">
-      <div className="resultat-film-component">
-        <img src={Film[0].image} alt="img" />
-        <button
-          type="button"
-          onClick={() => window.open(Film[0].teasing, "_blank")}
-        >
-          VOIR LA BANDE ANNONCE
-        </button>
-      </div>
+    <div className="resultat-film-component">
+      <img src={Film[0].image} alt="img" />
+      <button
+        type="button"
+        onClick={() => window.open(Film[0].teasing, "_blank")}
+      >
+        VOIR LA BANDE ANNONCE
+      </button>
     </div>
   );
 }
