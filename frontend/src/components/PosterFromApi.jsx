a594fc49f27e0f74402c6acd64e8a62e83ce2871
ClementPremartin <clementpremartin@gmail.com>
2022-04-28 11:44:57 +0200

    map images from api

--- frontend/src/components/PosterFromApi.jsx
+++ frontend/src/components/PosterFromApi.jsx
@@ -8,106 +8,11 @@ function ApiRequests({ totalUrlPosters }) {
     <div className="global">
       <div className="wrapper">
         <Carousel>
-          <div>
-            <img className="item" src={totalUrlPosters[0]} alt="movie poster" />
-          </div>
-          <div>
-            <img className="item" src={totalUrlPosters[1]} alt="movie poster" />
-          </div>
-          <div>
-            <img className="item" src={totalUrlPosters[2]} alt="movie poster" />
-          </div>
-          <div>
-            <img className="item" src={totalUrlPosters[3]} alt="movie poster" />
-          </div>
-          <div>
-            <img className="item" src={totalUrlPosters[4]} alt="movie poster" />
-          </div>
-          <div>
-            <img className="item" src={totalUrlPosters[5]} alt="movie poster" />
-          </div>
-          <div>
-            <img className="item" src={totalUrlPosters[6]} alt="movie poster" />
-          </div>
-          <div>
-            <img className="item" src={totalUrlPosters[7]} alt="movie poster" />
-          </div>
-          <div>
-            <img className="item" src={totalUrlPosters[8]} alt="movie poster" />
-          </div>
-          <div>
-            <img className="item" src={totalUrlPosters[9]} alt="movie poster" />
-          </div>
-          <div>
-            <img
-              className="item"
-              src={totalUrlPosters[10]}
-              alt="movie poster"
-            />
-          </div>
-          <div>
-            <img
-              className="item"
-              src={totalUrlPosters[11]}
-              alt="movie poster"
-            />
-          </div>
-          <div>
-            <img
-              className="item"
-              src={totalUrlPosters[12]}
-              alt="movie poster"
-            />
-          </div>
-          <div>
-            <img
-              className="item"
-              src={totalUrlPosters[13]}
-              alt="movie poster"
-            />
-          </div>
-          <div>
-            <img
-              className="item"
-              src={totalUrlPosters[14]}
-              alt="movie poster"
-            />
-          </div>
-          <div>
-            <img
-              className="item"
-              src={totalUrlPosters[15]}
-              alt="movie poster"
-            />
-          </div>
-          <div>
-            <img
-              className="item"
-              src={totalUrlPosters[16]}
-              alt="movie poster"
-            />
-          </div>
-          <div>
-            <img
-              className="item"
-              src={totalUrlPosters[17]}
-              alt="movie poster"
-            />
-          </div>
-          <div>
-            <img
-              className="item"
-              src={totalUrlPosters[18]}
-              alt="movie poster"
-            />
-          </div>
-          <div>
-            <img
-              className="item"
-              src={totalUrlPosters[19]}
-              alt="movie poster"
-            />
-          </div>
+          {totalUrlPosters.map((poster) => (
+            <div key={poster}>
+              <img className="item" src={poster} alt="movie poster" />
+            </div>
+          ))}
         </Carousel>
       </div>
     </div>
