69157bf4e085337dcdcea0f5eb5b9b7d4f33cba9
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-04-29 11:30:38 +0200

    fixing page resultat pizza

--- frontend/src/pages/ResultatPizza.jsx
+++ frontend/src/pages/ResultatPizza.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ButtonBackHome from "../components/ButtonBackHome";
+import ResultatPizzaComponent from "../components/ResultatPizzaComponent";
+import "./ResultatPizza.css";
 
 function ResultatPizza() {
   return (
     <div>
       <h1>Résultat pizzas</h1>
       <ButtonBackHome />
+      <div className="resultat-pizza">
+        <div>
+          <ResultatPizzaComponent />
+        </div>
+      </div>
     </div>
   );
 }
