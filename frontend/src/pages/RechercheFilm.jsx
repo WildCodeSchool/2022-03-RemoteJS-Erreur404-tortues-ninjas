a074ddf11d8ffffc073d8e72e44a0d80007925aa
WENDYBAQUE <BAQUE.WENDY@gmail.com>
2022-05-03 18:00:22 +0200

    Add data in DataPizza.js in date folder and resolve the conflict with the path of MoodLogo for pushing.

--- frontend/src/pages/RechercheFilm.jsx
+++ frontend/src/pages/RechercheFilm.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Title from "../components/Title";
-import MoodLogo from "../assets/moodlogo.png";
+import MoodLogo from "../assets/LogoMood.png";
 import ButtonBackHome from "../components/ButtonBackHome";
 import PosterFromApi from "../components/PosterFromApi";
 import GenreButton from "../components/GenreButtons";
