33fca98726e7c74ea22b5a5d1374aa1d9974fd82
WENDYBAQUE <BAQUE.WENDY@gmail.com>
2022-05-04 06:02:56 +0200

    Add popup page and his style and routes

--- frontend/src/pages/PopupRetourAccueil.jsx
+++ frontend/src/pages/PopupRetourAccueil.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import MoodLogo from "../assets/MoodLogo.png";
+
+import "./PopupRetourAccueil.css";
 
 function PopupRetourAccueil() {
   return (
     <div>
-      <h1>Retour à la page d accueil ?</h1>
+      <div className="MoodLogo">
+        <img src={MoodLogo} alt="MoodLogo" />
+      </div>
+      <div className="popup-wrapper">
+        <div className="popup-title">
+          <h1>
+            Votre choix est validé ! Merci de votre visite sur Mood et à bientôt
+            !
+          </h1>
+        </div>
+        <div className="popup-button">
+          <Link to="/home">
+            <button type="button"> Revenir à l accueil </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
