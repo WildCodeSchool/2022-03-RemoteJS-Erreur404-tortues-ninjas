77ae75a4f4214a2b151ad298cdbd5aa6036a25ee
ClementPremartin <clementpremartin@gmail.com>
2022-05-10 15:05:38 +0200

    adding algo in resultat film page

--- frontend/src/pages/ResultatFilm.jsx
+++ frontend/src/pages/ResultatFilm.jsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import ButtonBackHome from "../components/ButtonBackHome";
 import ButtonValidateResult from "../components/ButtonValidateResult";
 import ResultatFilmComponent from "../components/ResultatFilmComponent";
 
+import DataPizza from "../data/DataPizza";
 import MoodLogo from "../assets/MoodLogo.png";
+import { Context } from "../contexts/Context";
 
 function ResultatFilm() {
+  const { genreId } = useContext(Context);
+  const [pizzaRandom, setPizzaRandom] = useState({});
+  const filteredPizzas = DataPizza.filter(
+    (filteredPizza) => filteredPizza.categoryFilm === genreId
+  );
+
+  useEffect(() => {
+    setPizzaRandom(
+      filteredPizzas[Math.floor(Math.random() * filteredPizzas.length)]
+    );
+  }, []);
+
   return (
     <div>
       <div className="MoodLogo">
         <img src={MoodLogo} alt="MoodLogo" />
       </div>
-      <h1>Résultat films</h1>
-      <div>
-        <ResultatFilmComponent />
-      </div>
+      <ResultatFilmComponent />
+      <p>{pizzaRandom.phrase}</p>
       <ButtonValidateResult buttoncolor="bouton-valider-resultat-film" />
+      <div>
+        <img src={pizzaRandom.image} alt="une pizza" />
+        <button type="button">{pizzaRandom.name}</button>
+      </div>
       <ButtonBackHome />
     </div>
   );
