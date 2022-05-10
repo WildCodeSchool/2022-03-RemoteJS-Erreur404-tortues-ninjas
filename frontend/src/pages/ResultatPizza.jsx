a770c40f1d5d001ce95b5129ce7c2b9df6ce7279
ClementPremartin <clementpremartin@gmail.com>
2022-05-10 16:38:46 +0200

    adding resultat pizza algo

--- frontend/src/pages/ResultatPizza.jsx
+++ frontend/src/pages/ResultatPizza.jsx
@@ -1,34 +1,35 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
-import { Context } from "../contexts/Context";
 
 import ButtonBackHome from "../components/ButtonBackHome";
 import ButtonValidateResult from "../components/ButtonValidateResult";
 import ResultatPizzaComponent from "../components/ResultatPizzaComponent";
 import MoodLogo from "../assets/MoodLogo.png";
+import DataPizza from "../data/DataPizza";
+import { Context } from "../contexts/Context";
 
 import "./ResultatPizza.css";
 
 function ResultatPizza() {
-  const { pizzaCategoryFilm } = useContext(Context);
+  const { genreId } = useContext(Context);
   const [filteredMovie, setFilteredMovie] = useState([]);
-  const [randomMovie, setRandomMovie] = useState({});
+  const filteredPizzas = DataPizza.filter(
+    (filteredPizza) => filteredPizza.category === genreId
+  );
+
   useEffect(() => {
     axios
       .get(
         `https://api.themoviedb.org/3/discover/movie?api_key=${
           import.meta.env.VITE_TMDB_KEY
-        }&with_genres=${pizzaCategoryFilm}`
+        }&with_genres=${filteredPizzas[0].categoryFilm}`
       )
       .then((res) => {
-        setFilteredMovie(res.data.results);
+        setFilteredMovie(
+          res.data.results[Math.floor(Math.random() * res.data.results.length)]
+        );
       });
-    setRandomMovie(
-      filteredMovie[Math.floor(Math.random() * filteredMovie.length)]
-    );
   }, []);
-
-  console.warn(randomMovie);
 
   return (
     <div>
@@ -38,6 +39,10 @@ function ResultatPizza() {
       <div className="resultat-pizza">
         <ResultatPizzaComponent />
       </div>
+      <img
+        src={`https://image.tmdb.org/t/p/w500${filteredMovie.poster_path}`}
+        alt="poster"
+      />
       <ButtonValidateResult buttoncolor="bouton-valider-resultat-pizza" />
       <ButtonBackHome />
     </div>
